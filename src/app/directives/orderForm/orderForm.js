83aa5094f88aa496814296a494b5452f7f2a6ba2
Jammie Mountz <jmmountz@gmail.com>
2017-03-11 12:21:28 -0800

    coupon codes

--- src/app/directives/orderForm/orderForm.js
+++ src/app/directives/orderForm/orderForm.js
@@ -4,7 +4,6 @@ const countries = [
   'United States',
   'Mexico',
   'Puerto Rico',
-  'United Arab Emirates',
   'Austria',
   'Italy',
   'Belgium',
@@ -71,54 +70,68 @@ class OrderForm {
     $scope.status = {
       submitting: false,
       error: false,
-      notComplete: false
+      notComplete: false,
+      gettingPrice: false
     };
     $scope.data = {
       howMany: 1,
-      shipping: null,
-      total: null,
       firstName: '',
       lastName: '',
       email: '',
       phone: '',
+      referral: '',
       address1: '',
       address2: '',
       city: '',
       state: '',
       zip: '',
       country: '',
-      stripeToken: ''
+      stripeToken: '',
+      discount: null,
+      petbot: null,
+      petbot_after_discount: null, //eslint-disable-line
+      petbot_unit_cost: null, //eslint-disable-line
+      petbot_unit_cost_after_discount: null, //eslint-disable-line
+      shipping: null,
+      total: null
     };
     $scope.submitForm = function () {
-      $http.post("https://pay.petbot.com/pay", JSON.stringify({form: $scope.data})).then(successCallback, errorCallback);
+      $http.post("https://pay.petbot.com/pay2", JSON.stringify({form: $scope.data})).then(paySuccessCallback, payErrorCallback);
     };
-    function successCallback() {
+    function paySuccessCallback() {
       $scope.status.submitting = false;
       fbq('track', 'Purchase'); // eslint-disable-line
       $location.path(`/thanks/${$scope.data.howMany}/${$scope.data.firstName}`);
     }
-    function errorCallback() {
+    function payErrorCallback() {
       $scope.status.submitting = false;
       const displayError = document.getElementById('error-content');
-      displayError.textContent = 'Error submitting information.';
+      displayError.textContent = 'Error submitting information! :( Please send us an email.';
       $scope.status.error = true;
     }
-    $scope.calculateTotal = function () {
-      $scope.data.shipping = $scope.data.howMany * 25;
-      if ($scope.data.howMany > 1) {
-        $scope.data.total = ((199 * $scope.data.howMany * 0.9) + $scope.data.shipping).toFixed(2);
-        $scope.discount = true;
-      } else { // eslint-disable-line
-        $scope.data.total = (199 + $scope.data.shipping).toFixed(2);
-        $scope.discount = false;
-      }
+    $scope.getTotal = function () {
+      $scope.status.gettingPrice = true;
+      $http.post("https://pay.petbot.com/getcost", JSON.stringify({form: $scope.data})).then(costSuccessCallback, costErrorCallback);
     };
+    function costSuccessCallback(response) {
+      $scope.status.gettingPrice = false;
+      $scope.data.discount = response.data.discount ? response.data.discount.toFixed(2) : null;
+      $scope.data.petbot = response.data.petbot ? response.data.petbot.toFixed(2) : null;
+      $scope.data.petbot_after_discount = response.data.petbot_after_discount ? response.data.petbot_after_discount.toFixed(2) : null; //eslint-disable-line
+      $scope.data.petbot_unit_cost = response.data.petbot_unit_cost ? response.data.petbot_unit_cost.toFixed(2) : null; //eslint-disable-line
+      $scope.data.petbot_unit_cost_after_discount = response.data.petbot_unit_cost_after_discount ? response.data.petbot_unit_cost_after_discount.toFixed(2) : null; //eslint-disable-line
+      $scope.data.shipping = response.data.shipping ? response.data.shipping.toFixed(2) : null;
+      $scope.data.total = response.data.total ? response.data.total.toFixed(2) : null;
+    }
+    function costErrorCallback() {
+      $scope.data.total = 'Error!';
+    }
   }
 
   link(scope) {
     const elements = scope.stripe.elements();
     const card = elements.create('card');
-    scope.calculateTotal();
+    scope.getTotal();
 
     card.mount('#card-element');
     card.addEventListener('change', ({error}) => {
@@ -131,7 +144,6 @@ class OrderForm {
       }
     });
 
-    // const form = document.getElementById('payment-form');
     scope.submitOrderForm = () => {
       scope.status.submitting = true;
       scope.stripe.createToken(card).then(result => {
